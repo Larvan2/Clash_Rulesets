a3428ebe869d292ff1e09e030171d9274450c563
N1klaz <78135608+N1klaz@users.noreply.github.com>
2021-11-12 13:45:11 +0800

    fix

--- generators/downloader.go
+++ generators/downloader.go
@@ -3,9 +3,11 @@ package generators
 import (
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func DownloadList(url, fileName string) {
+	os.Mkdir("rules", 0777)
 	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
